9185569b718677ada082ba5a135e680abb161bbe
tsdmkp <t9@bk.ru>
2025-04-28 13:23:30 +0300

    online

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // base: '/idle-garage-game/', // <-- УБИРАЕМ или комментируем эту строку для Vercel
+
+  server: {
+    port: 5173,
+    open: true,
+    // proxy неТо нужен, если обращаемся к бэкенду по полному URL
+  },
+
+  build: {
+    outDir: 'dist',
+  }
 })
